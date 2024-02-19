ecd6f4a639cae3107439391e53b76d190426bbb2
이홍열 <hy@codnut.com>
2024-02-19 15:21:52 +0900

    jwt strategy에 옵션값 명시 추가

--- src/auth/security/passport.jwt.strategy.ts
+++ src/auth/security/passport.jwt.strategy.ts
@@ -14,6 +14,8 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
       //ignoreExpiration => 특정 상황에서 만료 시간을 무시하고 토큰을 계속해서 검증하고 싶다면 ignoreExpiration을 true로 설정. 개발 환경에서 토큰 만료 시간을 무시하고 접근이 가능.
       //false이면, 만료된 토큰의 경우 인증이 실패됨.
       ignoreExpiration: true,
+      algorithms: ['HS256'],
+      type: 'jwt', //타입을 명시적으로 작성(이외의 타입 : 'local', 'jwt', 'bearer', 'basic' 등)
       secretOrKey: `${process.env.JWT_SECRET_KEY}` || 'jwtSecretKey' //, => auth.Module에서 register()에 secret의 value값과 일치해야함.
     })
   }
