f2d5bdce95fb03ce3eb54396a93ff09f33eac2d8
이홍열 <hy@codnut.com>
2024-02-22 14:04:41 +0900

    user 데코레이터 생성

---
+++ src/auth/user.decorator.ts
@@ -0,0 +1,32 @@
+import { ExecutionContext, createParamDecorator } from "@nestjs/common";
+import { Payload } from "./security/user.payload.interface";
+
+export const User = createParamDecorator(
+  (data: any, context: ExecutionContext) => {
+    const req = context.switchToHttp().getRequest()
+    return getJwtFromRequest(req)
+  }
+)
+
+function getJwtFromRequest(req) {
+  let token = null
+  if(req && req.cookies) {
+    token = req.cookies['accessToken']
+    if(!token) {
+      const {authorization} = req.headers
+      //왜 authorization을 구조분해할당으로 사용했는지에 대해서 공부해야할 필요가 있습니다.
+      if(!authorization) return token
+      token = authorization.replace('Bearer ', '')
+    }
+    const base64Payload = token.split('.')[1]
+    const payloadBuffer = Buffer.from(base64Payload, 'base64')
+  
+    const payload: Payload = JSON.parse(payloadBuffer.toString())
+  
+    if(payload) {
+      return payload
+    }
+
+    return null
+  }
+}
