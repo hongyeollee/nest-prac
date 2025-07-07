1bcd0e5c6c78971689d52b2e3921878a789753bc
이홍열 <hy@codnut.com>
2025-07-07 16:56:11 +0900

    add: 스웨거 기능 추가

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,26 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import * as cookieParser from 'cookie-parser';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import * as cookieParser from "cookie-parser";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
-  const port = 3000
-  
-  const app = await NestFactory.create(AppModule);
+  const port = 3000;
 
-  app.use(cookieParser())
-  
-  await app.listen(port)
-  console.log(`server listen on port ${port}🚀`)
+  const app = await NestFactory.create(AppModule);
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle("nest prac")
+    .setDescription("나홀로 nest")
+    .setVersion("1.0.0")
+    .addTag("nest prac")
+    .build();
+
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup("api/api-docs", app, documentFactory);
+
+  app.use(cookieParser());
+
+  await app.listen(port);
+  console.log(`server listen on port ${port}🚀`);
 }
 bootstrap();
