107985325d897e1f88f19a0ba1c600ea0935be40
이홍열 <hy@codnut.com>
2025-07-07 16:44:47 +0900

    refactor: 사용자 회원가입 리팩토링 - 엔티티   - 클래스 틀랜스포머 적용   - date컬럼에 사용에 맞도록 데코레이터 수정 - dto   - createUserDTO 추가 - 사용자 회원가입 스웨거에 맞도록 추가 및 리턴시 엔틔티 클래스 트랜스포머 적용 - then 절 제거 및 리턴값 수정

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,41 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsNotEmpty, IsString, Min } from "class-validator";
+import { ResponseCommonSuccessDTO } from "src/_common/_dto/common-success-response.dto";
+
+export class CreateUserDTO {
+  @IsString()
+  @IsNotEmpty()
+  @Min(2)
+  @ApiProperty({
+    name: "name",
+    example: "김아무개",
+    required: true,
+    nullable: false,
+    description: "사용자 이름",
+    minLength: 2,
+  })
+  name: string;
+
+  @IsString()
+  @IsEmail()
+  @IsNotEmpty()
+  @ApiProperty({
+    name: "사용자 이메일주소",
+    example: "abcd@efgh.com",
+    required: true,
+    nullable: false,
+    description: "사용자 이메일",
+  })
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    name: "password",
+    example: "Avnf1@8ng",
+    required: true,
+    nullable: false,
+    description: "사용자 비밀번호",
+  })
+  password: string;
+}
