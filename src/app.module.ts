0ac78b5871220009e2bb26bf7c381a8eebdeb1de
이홍열 <hy@codnut.com>
2025-07-15 17:01:27 +0900

    fix: typeorm host 및 logging 값 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,7 @@ import { EmailModule } from "./mail/mail.module";
   imports: [
     TypeOrmModule.forRoot({
       type: "mysql",
-      host: "localhost",
+      host: process.env.DB_HOST || "localhost",
       port: 3306,
       username: process.env.DB_USER_NAME || "root",
       password: process.env.DB_PASSWORD || "ghdfuf2", //"root",
@@ -21,7 +21,7 @@ import { EmailModule } from "./mail/mail.module";
       entities: [User, Post],
       synchronize: false,
       timezone: "Asia/Seoul", //'Z' => UTC
-      logging: true, //개발환경에서 유용하게 활용함.
+      logging: false, //개발환경에서 유용하게 활용함.
       // logging: ['error', 'warn'] //운영 환경에서는 에러위주, 추가적으로 하면 경고까지도 하는 경우가 일반적인것 같음(chat GPT 내용 확인)
     }),
 
