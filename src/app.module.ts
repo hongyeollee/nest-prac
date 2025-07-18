d5d0ab233fd7cca71bccc3eb1458045b4e598ee3
이홍열 <hy@codnut.com>
2025-07-18 13:32:51 +0900

    fix: typeorm timezone 적용여부에 대한 설명 주석 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -29,6 +29,13 @@ import { ConfigModule } from "@nestjs/config";
        * 'local': 서버의 로컬시간대
        * 'Z': UT
        * '+09:00': UTC+9, 한국시간
+       * [참고사항]: timezone은 typeorm에서 적용된만큼 typeorm 컬럼을 사용해야 DB에 적용받음
+       * ex. @Column(), @CreateDateColumn() 등과 같이 사용된 경우.
+       * [요약]
+       * 동작방식: @UpdateDateColum(), @Colum({ type: 'datetime' }) 수동 설정 후 newDate()로 저장
+       * => timezone 적용됨
+       * 동작방식: dataSource.query('SELECT now()'), new Date()
+       * => timezone 적용 안 됨
        */
       timezone: "+09:00",
       logging: false, //개발환경에서 유용하게 활용함.
