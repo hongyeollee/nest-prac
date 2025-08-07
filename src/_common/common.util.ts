e32ccbb7d4f85da2b94a3d8f6b0f96bdd228b7b4
이홍열 <zeler1004@gmail.com>
2025-08-07 13:24:51 +0900

    fix: generateRandomString 함수에서 옵셔널 파라미터 추가 수정(별도의 파라미터를 받을때 옵셔널 파라미터 기준으로 생성될 수 있도록 수정)

--- src/_common/common.util.ts
+++ src/_common/common.util.ts
@@ -5,11 +5,12 @@ export class CommonUtil {
   private strings =
     "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890!@#$%^&*()";
 
-  generateRandomString(length: number): string {
+  generateRandomString(length: number, customParameter?: string): string {
     let result: string = "";
+    customParameter = customParameter || this.strings;
     for (let i = 0; i < length; i++) {
-      const randomIdx = Math.floor(Math.random() * this.strings.length);
-      result += this.strings.charAt(randomIdx);
+      const randomIdx = Math.floor(Math.random() * customParameter.length);
+      result += customParameter.charAt(randomIdx);
     }
 
     return result;
