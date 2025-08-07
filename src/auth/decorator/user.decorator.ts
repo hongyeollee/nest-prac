f54edd1f0ce6bac71d703b0d1551767ce8d22fba
이홍열 <zeler1004@gmail.com>
2025-08-07 13:23:12 +0900

    fix: User 데코레이터 request에서 유저정보 찾아올 수 있도록 수정

--- src/auth/decorator/user.decorator.ts
+++ src/auth/decorator/user.decorator.ts
@@ -2,25 +2,35 @@ import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 import { Payload } from "../security/user.payload.interface";
 
 export const User = createParamDecorator(
-  (data: any, context: ExecutionContext) => {
-    const request = context.switchToHttp().getRequest();
-    return getJwtfromRequest(request);
+  (data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    return request.user;
   },
 );
 
-const getJwtfromRequest = (request) => {
-  let token = null;
-  if (request && request.cookies) {
-    token = request.cookies["test"];
-    if (!token) {
-      const { authorization } = request.headers;
-      if (!authorization) return token;
-      token = authorization.replace("Bearer", "");
-    }
-  }
+/**
+ * 주석부분 레거시
+ */
+// export const User = createParamDecorator(
+//   (data: any, context: ExecutionContext) => {
+//     const request = context.switchToHttp().getRequest();
+//     return getJwtfromRequest(request);
+//   },
+// );
 
-  const base64Payload = token.split(".")[1];
-  const payloadBuffer = Buffer.from(base64Payload, "base64");
-  const payload: Payload = JSON.parse(payloadBuffer.toString()) as Payload;
-  return payload;
-};
+// const getJwtfromRequest = (request) => {
+//   let token = null;
+//   if (request && request.cookies) {
+//     token = request.cookies["test"];
+//     if (!token) {
+//       const { authorization } = request.headers;
+//       if (!authorization) return token;
+//       token = authorization.replace("Bearer", "");
+//     }
+//   }
+
+//   const base64Payload = token.split(".")[1];
+//   const payloadBuffer = Buffer.from(base64Payload, "base64");
+//   const payload: Payload = JSON.parse(payloadBuffer.toString()) as Payload;
+//   return payload;
+// };
