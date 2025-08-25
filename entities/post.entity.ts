679a2a540f008e511a2d88c0adb02e11d2ecabe9
이홍열 <zeler1004@gmail.com>
2025-08-25 22:05:08 +0900

    fix: post entity에서 update, delete column에 nullable 추가

--- entities/post.entity.ts
+++ entities/post.entity.ts
@@ -49,17 +49,19 @@ export class PostEntity {
     name: "updatedDt",
     default: () => "CURRENT_TIMESTAMP(0)",
     onUpdate: "CURRENT_TIMESTAMP(0)",
+    nullable: true,
     comment: "게시글 데이터 수정일",
   })
-  updatedDt: Date;
+  updatedDt: Date | null;
 
   @Column("datetime", {
     precision: 0,
     name: "deletedDt",
     default: () => "CURRENT_TIMESTAMP",
+    nullable: true,
     comment: "게시글 데이터 삭제일",
   })
-  deletedDt: Date;
+  deletedDt: Date | null;
 
   //관계설정
   @ManyToOne(() => UserEntity, (user) => user.posts, {
