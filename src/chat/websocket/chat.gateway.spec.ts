af2522ce5be28230496a33eb6e6cdef3ec450a6d
이홍열 <zeler1004@gmail.com>
2026-02-28 01:48:03 +0900

    fix(chat): gateway 테스트코드 영역 수정(테스트 미작동 디버깅)

--- src/chat/websocket/chat.gateway.spec.ts
+++ src/chat/websocket/chat.gateway.spec.ts
@@ -1,13 +1,33 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { ChatGateway } from "./chat.gateway";
-// import { ChatService } from "./chat.service";
+import { ChatRoomService } from "../service/chat-room.service";
+import { ChatMessageService } from "../service/chat-message.service";
+import { ChatEventService } from "../service/chat-event.service";
+import { WsJwtGuard } from "../guards/ws-jwt.guard";
+import { JwtService } from "@nestjs/jwt";
 
 describe("ChatGateway", () => {
   let gateway: ChatGateway;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      // providers: [ChatGateway, ChatService],
+      providers: [
+        ChatGateway,
+        WsJwtGuard,
+        {
+          provide: ChatRoomService,
+          useValue: { getActiveMemberOrThrow: jest.fn() },
+        },
+        {
+          provide: ChatMessageService,
+          useValue: { createMessage: jest.fn() },
+        },
+        {
+          provide: ChatEventService,
+          useValue: { bindServer: jest.fn() },
+        },
+        { provide: JwtService, useValue: { verify: jest.fn() } },
+      ],
     }).compile();
 
     gateway = module.get<ChatGateway>(ChatGateway);
